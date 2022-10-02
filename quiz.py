f63a1753a7698fa97787ddca77f5c6bb53e7624f
RDX <110017464+RDXMRX@users.noreply.github.com>
2022-10-02 01:29:21 -0700

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,8 @@
+while(True):
+    inp = int(input("Enter the number\n"))
+    if inp>100:
+        print("YOU ARE HACKED\n")
+        break
+    else:
+        print("Try Again\n")
+        continue
